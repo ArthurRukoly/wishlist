c584aabb9515043f3abd1ec39da60c2c2c95cb92
iferus <iferus000@gmail.com>
2025-01-28 14:12:04 +0200

    controller + model

--- wishlist-app/routes/web.php
+++ wishlist-app/routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WishController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return "yo";
+});
+
+Route::get('/wishlist', [WishController::class, 'Index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
